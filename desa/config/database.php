e7e180fa76e8117021664fe1e7ef60ff5931a4ec
BJ Setiawan <bill.jodh@gmail.com>
2020-03-02 13:26:11 +0700

    update desa

---
+++ desa/config/database.php
@@ -0,0 +1,16 @@
+<?php
+// -------------------------------------------------------------------------
+// Konfigurasi database dalam file ini menggantikan konfigurasi di file asli
+// SID di donjo-app/config/database.php.
+//
+// Letakkan username, password dan database sebetulnya di file ini.
+// File ini JANGAN di-commit ke GIT. TAMBAHKAN di .gitignore
+// -------------------------------------------------------------------------
+
+// Data Konfigurasi MySQL yang disesuaikan
+
+$db['default']['hostname'] = 'sid_mysql';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'fkd91ok3fngncx';
+$db['default']['database'] = 'sid';
+?>
